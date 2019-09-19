33f95d4d7f33b1a522b10c0e752f32f78a30c75d
Weng Chiyu <cyrain@gmail.com>
2019-09-19 14:49:59 +0800

    app.R to test shinyTree package in R

---
+++ shinyTree_test/app.R
@@ -0,0 +1,71 @@
+# shinyTree cannot render twice after re-open wellpanel
+library(shiny)
+library(shinyTree)
+
+ui <- fluidPage(
+   titlePanel("Test Shiny Tree"),
+   
+   sidebarLayout(
+      sidebarPanel(
+        actionButton("get_tree", "Tree list")
+      ),
+      
+      mainPanel(
+        uiOutput("test_ui"),
+        verbatimTextOutput("outText")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  treex <- structure(list(a=list(a1=1,a2=2) , b="b"), stopened = T)
+  treex <- lapply(treex, function(x) structure(x, stopened = T))
+
+  tree_selx <- eventReactive(input$tree_List,{
+    req(input$tree_List) 
+
+    return(unlist(get_selected(input$tree_List)))
+  })
+  
+  actionx <- reactiveValues(sel=character(), toggle=FALSE)
+  
+  observeEvent(input$get_tree,{
+    req(input$get_tree)
+    
+    isolate({
+      actionx$toggle <- as.logical(input$get_tree>0)
+    })
+  })
+  
+  observeEvent(input$tree_apply, {
+    actionx$sel <- as.character(tree_selx())
+    actionx$toggle <- FALSE
+  })
+  
+  output$outText <- renderText({
+    actionx$sel
+  })
+  
+  output$test_ui <- renderUI({
+    if (actionx$toggle) {
+      wellPanel(
+        id = "treePanel", 
+        shinyTree("tree_List", search=TRUE), 
+        actionButton("tree_apply", "Apply"),
+        tagList(tags$script(HTML(paste('document.getElementById(\"tree_apply\").onclick = function(){$(\"#treePanel\").hide();};',sep=""))),
+                tags$script(HTML(paste('document.getElementById(\"get_tree\").onclick= function(){$(\"#treePanel\").show();}',sep=""))))
+      )
+    }
+  })
+  
+  output$tree_List <- renderTree({
+    treex
+  })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
